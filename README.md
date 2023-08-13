37f54924522eb6400d5025ba5bc5d70e0f36ec86
Aiden Ly <atl.aidenly@gmail.com>
2023-08-12 22:39:06 -0400

    Base prototype for project

---
+++ README.md
@@ -0,0 +1,13 @@
+## Inspiration
+
+## What it does
+
+## How we built it
+
+## Challenges we ran into
+
+## Accomplishments
+
+## What we learned
+
+## What's next
---
+++ script.js
@@ -0,0 +1,33 @@
+function calculate() {
+  let height = document.getElementById('height').value;
+  let weight = document.getElementById('weight').value;
+  let sex = '';
+  try {
+    sex = document.querySelector('input[name="sex"]:checked').value;
+  }
+  catch (error) {
+    alert("Error: Sex not selected.");
+    return;
+  }
+
+  if (height === '' || height <= 0 || isNaN(height)) {
+    alert("Error: Invalid height! Make sure it is a number above 0.");
+    return;
+  }
+
+  if (weight === '' || weight <= 0 || isNaN(weight)) {
+    alert("Error: Invalid weight! Make sure it is a number above 0.");
+    return;
+  }
+
+  document.getElementsByClassName('interactive')[0].style.visibility = 'hidden';
+  
+  console.log(height, weight, sex)
+  
+}
+
+function clear() {
+  return;
+}
+
+
