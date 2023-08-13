93c256f26aff365e3dd54e642d6d3d38ae54d37d
Aiden Ly <atl.aidenly@gmail.com>
2023-08-13 00:35:37 -0400

    +Age and activity level, working on calculate()

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 function calculate() {
   let height = document.getElementById('height').value;
   let weight = document.getElementById('weight').value;
+  let age = document.getElementById('age').value;
+  let activity = parseFloat(document.getElementById('activity').value);
   let sex = '';
   try {
     sex = document.querySelector('input[name="sex"]:checked').value;
   }
   catch (error) {
     alert("Error: Sex not selected.");
+    return;
+  }
+  
+  if (age === '' || age <= 0 || age > 100 || isNaN(age)) {
+    alert("Error: Invalid age! Make sure it is a number between 0 and 100.");
     return;
   }
 
@@ -20,15 +27,18 @@ function calculate() {
     return;
   }
 
-  document.getElementsByClassName('interactive')[0].style.visibility = 'hidden';
   
-  console.log(height, weight, sex)
+
+  document.getElementsByClassName('interactive')[0].style.opacity = '0';
+  document.getElementsByClassName('interactive')[0].style.pointerEvents = 'none';
+  
+  document.getElementsByClassName('result')[0].style.opacity = '1';
+  document.getElementsByClassName('result')[0].style.pointerEvents = 'auto';
+  
+  console.log(height, weight, sex, activity, age)
   
 }
 
-function bmi_equation() {
-  return;
-}
 
 function clear() {
   return;
